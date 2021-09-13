684cbb7058289d97f536d296ae5ba17658a06fc7
handao-everfit <handao-everfit@users.noreply.github.com>
2021-09-13 12:22:06 +0000

    Deploying to gh-pages from  @ 311d0f29bb2bf7d103bf4fff9377f62072a68900 🚀

---
+++ precache-manifest.feb7429c9da50583666a82f392b28ed4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7a641a11fa42336dac7d54e87fbbac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df1f4c7037b252bac869",
+    "url": "/static/css/main.5e4d01b3.chunk.css"
+  },
+  {
+    "revision": "0f6448c4ade9f0e79786",
+    "url": "/static/js/2.7528ce4a.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.7528ce4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1f4c7037b252bac869",
+    "url": "/static/js/main.ef0ea0f1.chunk.js"
+  },
+  {
+    "revision": "c58f1ea9baa43354727f",
+    "url": "/static/js/runtime-main.665abb56.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/static/media/stanfordLogo.3b758ef5.png"
+  }
+]);
