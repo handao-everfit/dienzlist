d24d5c7c8a8483e443fb0a876e6b2170d141799f
Han Dao <handao@everfit.io>
2021-09-16 03:38:40 -0400

    doi ngu section

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -4,48 +4,45 @@ import "./ExperienceCard.scss";
 export default function ExperienceCard({cardInfo, isDark}) {
   return (
     <div className={isDark ? "card-box-dark" : "card-box"}>
-      <div class="cardBox">
-        <div class="card">
-          <div class="front">
-            <h3>Card One</h3>
-            <p>Hover to flip</p>
-            <strong>&#x21bb;</strong>
-          </div>
-          <div class="back">
-            <h3>Back Side One</h3>
-            <p>Content in card one</p>
+      <div class="card">
+        <div class="front">
+          <img
+            className="profile-pic"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN7lfwGY5oRempqimCHtdQJqQFNVp-lXP0Rg&usqp=CAU"
+            alt="grandma"
+          />
+        </div>
+        <div class="back">
+          <div className="experience-text-details">
+            <h5
+              className={
+                isDark
+                  ? "experience-text-role dark-mode-text"
+                  : "experience-text-role"
+              }
+            >
+              {cardInfo.role}
+            </h5>
+            <h5
+              className={
+                isDark
+                  ? "experience-text-date dark-mode-text"
+                  : "experience-text-date"
+              }
+            >
+              {cardInfo.date}
+            </h5>
+            <p
+              className={
+                isDark
+                  ? "subTitle experience-text-desc dark-mode-text"
+                  : "experience-text-desc"
+              }
+            >
+              {cardInfo.desc}
+            </p>
           </div>
         </div>
-      </div>
-
-      <div className="experience-text-details">
-        <h5
-          className={
-            isDark
-              ? "experience-text-role dark-mode-text"
-              : "experience-text-role"
-          }
-        >
-          {cardInfo.role}
-        </h5>
-        <h5
-          className={
-            isDark
-              ? "experience-text-date dark-mode-text"
-              : "experience-text-date"
-          }
-        >
-          {cardInfo.date}
-        </h5>
-        <p
-          className={
-            isDark
-              ? "subTitle experience-text-desc dark-mode-text"
-              : " experience-text-desc"
-          }
-        >
-          {cardInfo.desc}
-        </p>
       </div>
     </div>
   );
